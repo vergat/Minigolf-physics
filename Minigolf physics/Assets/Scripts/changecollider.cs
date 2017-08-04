48a47ca0a066a4c0e0d32eb60680680964542a86
gianluca <gianluca.goberti@gmail.com>
2017-08-04 18:33:42 +0200

    -Created new folder Materials Scenes and Scripts. -Created new Physic Material and Material. -Created a main Scene. -Created new Scripts DragMouseOrbit(camera orbit on ball),  Test(cube hit the ball), sphere(dellete this script) and changecollider(stop the cube after hit the ball).

---
+++ Minigolf physics/Assets/Scripts/changecollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changecollider : MonoBehaviour {
+    [SerializeField]
+    private GameObject meh;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject != meh)
+        {
+            meh.gameObject.GetComponent<Rigidbody>().velocity=Vector3.zero;
+            meh.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+            Debug.Log("beh");
+        }
+    }
+}
---
+++ Minigolf physics/Assets/Scripts/sphere.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphere : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 offset=Vector3.zero;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+
+       // Camera.main.transform.position = gameObject.transform.position + offset;
+    }
+}
